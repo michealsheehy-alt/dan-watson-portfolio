b79863c2d9b577335d6f4ba107655f595c4f2858
michealsheehy-alt <michealsheehy@gmail.com>
2026-07-30 19:08:35 -0700

    Load second DWTS TikTok clip on portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="/additional-dwts-clips.js" strategy="afterInteractive" />
       </body>
     </html>
   );
